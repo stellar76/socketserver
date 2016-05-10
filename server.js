3fdf06fe16a5b859d61a153350d1577a823ddfef
jasjar <jasjar@gmail.com>
2016-05-10 15:02:19 -0500

    Updates using Chokidar instead of FS
    And other stuff and things.

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var jf = require('jsonfile'); 
-var fs = require('fs');
+var jf = require('jsonfile');
+var chokidar = require('chokidar');
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
 io.sockets.on('connection', function(socket) {
-    fs.watch("data.json", function(event, fileName) { //watching data.json file for any changes
-        jf.readFile('data.json', function(err, data) { //if change detected read the data.json file
-            var data = data; //store data.json data in a var
-            console.log(data);
-            console.log('updated JSON data') //just for debugging if there's a problem
-            socket.volatile.emit('notification', data); //emit the data to all clients when data is updated
-        });
+    var _nowPlayingFile = '\\\\13.1.1.81\\d$\\websites\\umbAFR\\nowPlaying.json';
+
+    //initial notification
+    sendNowPlayingToClient();
+
+    //watch for file change
+    var watcher = chokidar.watch(_nowPlayingFile, {});
+    watcher.on('change', function(file) {
+        //when file changes, send subsequent notifications
+        sendNowPlayingToClient();
     });
+
+    //function sends nowplaying notification data to client
+    function sendNowPlayingToClient() {
+        //read the file
+        jf.readFile(_nowPlayingFile, function(err, data) {
+            //emit the data to all clients when data is updated
+            socket.emit('nowPlaying', data);
+        });
+    }
 });
+
 http.listen(3030, function() { //socket io listening on port 3030
-    console.log('listening on *:3030');
+    console.log('listening for socket connection(s) on *:3030');
 });
-//
+
 var express = require('express'),
-check = express(); 
+    check = express();
 check.use('/', express.static(__dirname + '/'));
-check.listen(8000, function() { //socket io listening on port 3030
+check.listen(8000, function() {
     console.log('webserver listening on *:8000');
 });
